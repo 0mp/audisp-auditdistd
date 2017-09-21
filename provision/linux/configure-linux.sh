bbb2d1e85f8d3a6dd934d1a6918b8948df1adaba
Mateusz Piotrowski <mpp302@gmail.com>
2017-09-21 17:55:26 +0200

    Fix Linux configuration

--- provision/linux/configure-linux.sh
+++ provision/linux/configure-linux.sh
@@ -3,7 +3,7 @@
 required=
 for dep in libssl-dev byacc flex auditd
 do
-    if dpkg-query -l "$dep" 1>/dev/null 2>&1
+    if ! dpkg-query -l "$dep" 1>/dev/null 2>&1
     then
         required="$required $dep"
     fi
@@ -14,7 +14,7 @@ then
     apt-get -y install $required
 fi
 
-if ! grep ^auditdistd /etc/passwd
+if ! grep ^auditdistd /etc/passwd 1>/dev/null
 then
     useradd -m auditdistd
 fi
